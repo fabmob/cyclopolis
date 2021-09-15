1378645c7aa4ac6ab487e97366c90549545d5d47
Mael <mael.thomas@data.gouv.fr>
2021-09-15 17:50:51 +0200

    Police Rubik

---
+++ components/Indicator.js
@@ -0,0 +1,123 @@
+import Link from 'next/link'
+import Emoji from './Emoji'
+import { formatInputNumber } from '../pages/villes/[name]'
+import Context from './Context'
+import { useState } from 'react'
+
+export default ({ meta: [key, { label, icon, unit: unitRaw }], data }) => {
+  const [indicator, setIndicator] = useState(key)
+  const [number, unit] = formatInputNumber(data[indicator], unitRaw)
+
+  return (
+    <Link href={'/indicateurs/' + indicator}>
+      <li
+        title="Comparer avec d'autres villes"
+        css={`
+          cursor: pointer;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-wrap: wrap;
+          position: relative;
+          padding-bottom: 1rem;
+          h2 {
+            text-transform: uppercase;
+            font-weight: 300;
+            margin: 0.4rem;
+            font-size: 100%;
+          }
+          max-width: 14rem;
+          h2 {
+            max-width: 8rem;
+            text-align: right;
+          }
+          @media (max-width: 800px) {
+            max-width: 8rem;
+            h2 {
+              font-size: 90%;
+              text-align: center;
+              max-width: auto;
+              text-align: center;
+            }
+            .emoji {
+              font-size: 150%;
+            }
+          }
+          box-shadow: 0 1px 3px rgba(209, 41, 41, 0.12),
+            0 1px 2px rgba(209, 41, 41, 0.24);
+          border: 2px solid black;
+          border-radius: 0.2rem;
+          margin: 0.6rem 0;
+        `}
+      >
+        <div className="emoji" css=" margin: 0 .6rem;">
+          <Emoji e={icon} sizeRem="3" />
+        </div>
+        <h2>{label} </h2>
+        {indicator.includes('Distance moyenne') && (
+          <div
+            css={`
+              width: 100%;
+              display: flex;
+              justify-content: center;
+              button {
+                cursor: zoom-in;
+                margin: 0 0.4rem;
+                border: 1px solid #444;
+                padding: 0 0.2rem;
+                line-height: 1rem;
+                border-radius: 0.2rem;
+                color: #444;
+              }
+            `}
+          >
+            <button
+              css={`
+                ${indicator === 'Distance moyenne en semaine'
+                  ? 'background: #333; color: white !important;'
+                  : ''};
+              `}
+              onClick={(e) => {
+                setIndicator(
+                  indicator !== 'Distance moyenne en semaine'
+                    ? 'Distance moyenne en semaine'
+                    : 'Distance moyenne'
+                )
+                e.preventDefault()
+                e.stopPropagation()
+              }}
+            >
+              semaine
+            </button>
+            <button
+              css={`
+                ${indicator === 'Distance moyenne en week-end'
+                  ? 'background: #333; color: white !important;'
+                  : ''};
+              `}
+              onClick={(e) => {
+                setIndicator(
+                  indicator !== 'Distance moyenne en week-end'
+                    ? 'Distance moyenne en week-end'
+                    : 'Distance moyenne'
+                )
+
+                e.preventDefault()
+                e.stopPropagation()
+              }}
+            >
+              weekend
+            </button>
+          </div>
+        )}
+
+        <div>
+          <span css="font-size: 200%">{number}</span>
+          &nbsp;
+          {unit}
+        </div>
+        <Context {...{ value: data[indicator], metric: indicator }} />
+      </li>
+    </Link>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,42 @@ html,
 body {
   padding: 0;
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+  font-family: Rubik, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   line-height: 1.6;
   font-size: 18px;
 }
+
+/* rubik-300 - latin */
+@font-face {
+  font-family: 'Rubik';
+  font-style: normal;
+  font-weight: 300;
+  src: local(''),
+       url('/fonts/rubik-v14-latin-300.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/fonts/rubik-v14-latin-300.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+
+/* rubik-regular - latin */
+@font-face {
+  font-family: 'Rubik';
+  font-style: normal;
+  font-weight: 400;
+  src: local(''),
+       url('/fonts/rubik-v14-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/fonts/rubik-v14-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+
+/* rubik-600 - latin */
+@font-face {
+  font-family: 'Rubik';
+  font-style: normal;
+  font-weight: 600;
+  src: local(''),
+       url('/fonts/rubik-v14-latin-600.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('/fonts/rubik-v14-latin-600.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+
 
 * {
   box-sizing: border-box;
