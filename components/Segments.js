ae97b1570039994dd7a44a0a9be34de6d9e75772
Mael <mael.thomas@data.gouv.fr>
2021-09-24 11:14:33 +0200

    Segment qui dépasse sur mobile

--- components/Segments.js
+++ components/Segments.js
@@ -56,6 +56,10 @@ export const SegmentName = styled.span`
   margin-left: 0.6rem;
   border-radius: 0.5rem;
   padding: 0.1rem 1rem;
+  @media (max-width: 800px) {
+    max-width: 12rem;
+    display: inline-block;
+  }
 `
 
 export const AllSegments = ({ data }) =>
