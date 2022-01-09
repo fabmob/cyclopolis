845b14ef9ea69b96fd01e134fda8119f15870c91
Tristram Gräbener <tristramg@gmail.com>
2022-01-09 11:58:34 +0100

    Add svgo config

---
+++ svgo.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            collapseGroups: false,
+            cleanupIDs: false,
+            removeViewBox: false,
+          },
+        },
+      },
+    ],
+  };
