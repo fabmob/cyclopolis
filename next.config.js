00fe17004ce3cf460f8496cc0a5d01eb7dc38a66
Tristram Gräbener <tristramg@gmail.com>
2022-01-09 11:54:36 +0100

    Fix deprecation warning

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 // This source was taken from the @next/mdx plugin source:
 // https://github.com/vercel/next.js/tree/canary/packages/next-mdx
 module.exports = {
-  target: "serverless",
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.csv/,
