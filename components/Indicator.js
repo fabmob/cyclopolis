646af73fe2f4991571873cfa873c2bfd76caff71
Tristram Gräbener <tristramg@gmail.com>
2022-01-14 13:36:08 +0100

    Ajout des évolutions par ville

--- components/Indicator.js
+++ components/Indicator.js
@@ -1,8 +1,19 @@
 import Link from 'next/link'
 import Emoji from './Emoji'
-import { formatInputNumber } from '../pages/villes/[name]'
+import { formatInputNumber, rawToNumber } from '../pages/villes/[name]'
 import Context from './Context'
 import { useState } from 'react'
+
+function evolution(current, previous) {
+  if(previous === '') {
+    return null
+  } else {
+    const val = rawToNumber(previous)
+    const pct = Math.round((current - val) * 100 / current)
+    const sign = pct > 0 ? '+' : ''
+    return `(${sign}${pct} %)`
+  }
+}
 
 const Indicator = ({
   meta: [key, { color, label, icon, unit: unitRaw }],
@@ -10,6 +21,7 @@ const Indicator = ({
 }) => {
   const [indicator, setIndicator] = useState(key)
   const [number, unit] = formatInputNumber(data[indicator], unitRaw)
+  const evol = evolution(data[indicator], data[indicator + '_prec'])
 
   const selected = {
     background: '#333',
@@ -60,6 +72,7 @@ const Indicator = ({
         <div>
           <span style={{fontSize: '200%'}}>{number}</span>
           <span style={{marginLeft: '.4rem'}}>{unit}</span>
+          <span style={{fontSize: '50%'}}>{evol}</span>
         </div>
         <Context
           value={data[indicator]}
