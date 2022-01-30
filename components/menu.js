80259a6af931a19cfb315523c6e1e9bbbccbf5a7
Tristram Gräbener <tristramg@gmail.com>
2022-01-30 21:54:06 +0100

    Style the header menu

--- components/menu.js
+++ components/menu.js
@@ -1,22 +1,25 @@
 import Link from 'next/link'
 
 function className(page, link) {
-  if (page === link) return 'tab-button active-page'
-  else return 'tab-button'
+  if (page === link) return 'active-page'
+  else return ''
 }
 
 const Menu = ({ page }) => {
   return (
     <div className="menu">
-      <button className={className(page, 'territoires')}>
+      <div className={className(page, 'territoires')}>
         <Link href="/">Territoires</Link>
-      </button>
-      <button className={className(page, 'indicateurs')}>
+        <div></div>
+      </div>
+      <div className={className(page, 'indicateurs')}>
         <Link href="/indicateurs/vitesse">Indicateurs</Link>
-      </button>
-      <button className={className(page, 'a-propos')}>
+        <div></div>
+      </div>
+      <div className={className(page, 'a-propos')}>
         <Link href="/a-propos">À propos</Link>
-      </button>
+        <div></div>
+      </div>
     </div>
   )
 }
